d67be5c50fe8b30fc9569d1b4ac579ff9b06c44c
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-01 18:07:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-  session_start();
+//   session_start();
 
-  require 'db.php';
+//   require 'db.php';
 
-  if (isset($_SESSION['user_id'])) {
-    $records = $conn->prepare('SELECT id, email, password FROM usuarios WHERE id = :id');
-    $records->bindParam(':id', $_SESSION['user_id']);
-    $records->execute();
-    $results = $records->fetch(PDO::FETCH_ASSOC);
+//   if (isset($_SESSION['user_id'])) {
+//     $records = $conn->prepare('SELECT id, email, password FROM usuarios WHERE id = :id');
+//     $records->bindParam(':id', $_SESSION['user_id']);
+//     $records->execute();
+//     $results = $records->fetch(PDO::FETCH_ASSOC);
 
-    $user = null;
+//     $user = null;
 
-    if (count($results) > 0) {
-      $user = $results;
-    }
-  }
+//     if (count($results) > 0) {
+//       $user = $results;
+//     }
+//   }
 ?>
 
 
