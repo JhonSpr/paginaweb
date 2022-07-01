739f7f41e5faf4fe8362fd0d35ab1a5e2000defa
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-01 18:05:03 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,15 @@
-
-
 <?php
 
-$server = 'localhost';
-$username = '';
-$password = '';
-$database = '';
-
-try {
-  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
-} catch (PDOException $e) {
-  die('Connection Failed: ' . $e->getMessage());
-}
+// $server = 'localhost';
+// $username = '';
+// $password = '';
+// $database = '';
+ -->
+// try {
+//   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+// } catch (PDOException $e) {
+//   die('Connection Failed: ' . $e->getMessage());
+// }
 
 ?>
+ -->
