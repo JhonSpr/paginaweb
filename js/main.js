e6454a7953fb73163daf1af5db254304a267153f
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-13 00:13:59 -0500

    a

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ addEventListener('DOMContentLoaded', () => {
 function search_animal() {
     let input = document.getElementById('inputSearch').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('search');
+    let x = document.getElementsByClassName('animes');
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
