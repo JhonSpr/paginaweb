d79e96225f96596f82053e058bdbab75a4e751dd
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-12 23:31:21 -0500

    a

--- js/main.js
+++ js/main.js
@@ -16,29 +16,35 @@ addEventListener('DOMContentLoaded', () => {
 inputSearch = document.getElementById('inputSearch');
 box_search =  document.getElementById('box-search');
 
-document.getElementById('inputSearch').addEventListener('keyup' ,buscador());
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
 
-function buscador(){
+function buscador_interno(){
+
+
     filter = inputSearch.value.toUpperCase();
-    li = box_search.getElementsByTagName('li');
+    li = box_search.getElementsByTagName("li");
 
-    
-    for (i = 0; i < li.lenght; i++){
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
 
-        a = li[i].getElementsByTagName('a')(0);
+        a = li[i].getElementsByTagName("a")[0];
         textValue = a.textContent || a.innerText;
 
+        if(textValue.toUpperCase().indexOf(filter) > -1){
 
-        if (textValue.toUpperCase().indexOf(filter) > -1 ) {
             li[i].style.display = "";
             box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
 
         }else{
             li[i].style.display = "none";
         }
+
     }
 
 
 
 }
-
