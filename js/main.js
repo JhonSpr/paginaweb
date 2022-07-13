9d40da4a4a2dddb159954586ba10ef69460b6ec6
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-12 21:19:58 -0500

    a

--- js/main.js
+++ js/main.js
@@ -9,3 +9,22 @@ addEventListener('DOMContentLoaded', () => {
     }
 
 })
+
+
+
+
+// JavaScript code
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('animals');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
