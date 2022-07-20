36a2c33b966f7e03f6fc049111d7f026c923b6c7
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-20 01:24:36 -0500

    a

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ addEventListener('DOMContentLoaded', () => {
     const btn_menu = document.querySelector('body');
     if (btn_menu) {
         btn_menu.addEventListener('click' , () =>{
-            const menu_items = document.querySelector('.menu_items')
-            menu_items.classList.toggle('hidden')
+            const menu_items_esconder = document.querySelector('.menu_items')
+            menu_items_esconder.classList.toggle('hidden')
         })
     }
 
