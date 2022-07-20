1d53aca576b06ca003fb448c8ac6bc7d4e713745
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-20 01:33:02 -0500

    a

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 addEventListener('DOMContentLoaded', () => {
 
-    const btn_menu = document.querySelector('.menu-bars','body');
+    const btn_menu = document.querySelector('.menu-bars');
     if (btn_menu) {
         btn_menu.addEventListener('click' , () =>{
             const menu_items = document.querySelector('.menu_items')
@@ -10,6 +10,17 @@ addEventListener('DOMContentLoaded', () => {
 
 })
 
+addEventListener('DOMContentLoaded', () => {
+
+    const btn_menu = document.querySelector('body');
+    if (btn_menu) {
+        btn_menu.addEventListener('click' , () =>{
+            const menu_items = document.querySelector('.menu_items')
+            menu_items.classList.toggle('show')
+        })
+    }
+
+})
 
 
 
