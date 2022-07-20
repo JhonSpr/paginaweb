00b5987a481d20ae7654e204990b155f0ebd1022
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-19 21:33:58 -0500

    a

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 addEventListener('DOMContentLoaded', () => {
 
-    const btn_menu = document.querySelector('.btn_menu');
+    const btn_menu = document.querySelector('.boton');
     if (btn_menu) {
         btn_menu.addEventListener('click' , () =>{
             const menu_items = document.querySelector('.menu_items')
