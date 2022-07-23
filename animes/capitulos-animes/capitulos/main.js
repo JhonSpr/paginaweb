da08ee5a99251a63292401bc2b82c8382fe00848
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-23 17:49:28 -0500

    a

--- animes/capitulos-animes/capitulos/main.js
+++ animes/capitulos-animes/capitulos/main.js
@@ -12,4 +12,93 @@ addEventListener('DOMContentLoaded', () => {
 
 
 
+function activado(){
+                           
+    var i = document.getElementById('divPrincipal');
+
+    if (i.style.display === "block") {
+        e.style.background="#ca705";
+        i.style.display="block";
+        e2.style.background="transparent";
+        e3.style.background="transparent";    
+    }
+   }
+
+    function myFunction() {
+            var e = document.getElementById('opcion1');
+            var e2 = document.getElementById('opcion2');
+            var e3 = document.getElementById('opcion3');
+            var x = document.getElementById("myDIV1");
+            var g = document.getElementById('myDIV2');
+            var i = document.getElementById('divPrincipal');
+            if (i.style.display === "none") {
+                i.style.display = "block";
+                x.style.display="none";
+                g.style.display="none";
+                e.style.background="#c47005"
+                e2.style.background="transparent"
+                e3.style.background="transparent"
+              
+
+            } else {
+            
+                i.style.display = "block";
+                x.style.display="none";
+                g.style.display="none";
+        
+            }
+        }
+
+
+    
+        function myFunction3() {
+            var e = document.getElementById('opcion1');
+            var e2 = document.getElementById('opcion2');
+            var e3 = document.getElementById('opcion3');
+            var x = document.getElementById("myDIV1");
+            var g = document.getElementById('myDIV2');
+            var i = document.getElementById('divPrincipal');
+            if (x.style.display === "none") {
+                x.style.display = "block";
+                i.style.display="none";
+                g.style.display="none";
+                e.style.background="transparent"
+                e2.style.background="transparent"
+                e3.style.background="#c47005"
+
+            } else {
+            
+                x.style.display = "block";
+                i.style.display="none";
+                g.style.display="none";
+                
+        
+            }
+        }
+   
+    
+        function myFunction2() {
+            var e = document.getElementById('opcion1');
+            var e2 = document.getElementById('opcion2');
+            var e3 = document.getElementById('opcion3');
+            var x = document.getElementById("myDIV1");
+            var g = document.getElementById('myDIV2');
+            var i = document.getElementById('divPrincipal');
+            if (g.style.display === "none") {
+                x.style.display = "none";
+                g.style.display="block";
+                i.style.display= "none";
+                e.style.background="transparent"
+                e2.style.background="#c47005"
+                e3.style.background="transparent"
+               
+            } else {
+                
+                x.style.display = "none";
+                g.style.display="block";
+                i.style.display= "none";       
+            }
+        }
+
+
 
