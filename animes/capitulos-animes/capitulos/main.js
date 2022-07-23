4e648ce2b869a37a865f2ce2de479964348bac1a
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-23 02:57:44 -0500

    a

--- animes/capitulos-animes/capitulos/main.js
+++ animes/capitulos-animes/capitulos/main.js
@@ -12,16 +12,16 @@ addEventListener('DOMContentLoaded', () => {
 
 
 
-$('ul.tabs li a:first').addClass('active');
-	$('.contenedor_iframe iframe').hide();
-	$('.contenedor_iframe iframe:first').show();
+// $('ul.tabs li a:first').addClass('active');
+// 	$('.contenedor_iframe iframe').hide();
+// 	$('.contenedor_iframe iframe:first').show();
 
-	$('ul.tabs li a').click(function(){
-		$('ul.tabs li a').removeClass('active');
-		$(this).addClass('active');
-		$('.contenedor_iframe iframe').hide();
+// 	$('ul.tabs li a').click(function(){
+// 		$('ul.tabs li a').removeClass('active');
+// 		$(this).addClass('active');
+// 		$('.contenedor_iframe iframe').hide();
 
-		var activeTab = $(this).attr('href');
-		$(activeTab).show();
-		return false;
-	});
+// 		var activeTab = $(this).attr('href');
+// 		$(activeTab).show();
+// 		return false;
+// 	});
