a4d3305768cc9e09eba5bb239a144b9e04e19fa4
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-23 20:22:25 -0500

    a

--- animes/capitulos-animes/capitulos/main.js
+++ animes/capitulos-animes/capitulos/main.js
@@ -86,20 +86,29 @@ addEventListener('DOMContentLoaded', () => {
            function pausarVideo(){
             if(i.style.display === "none"){
                 window.frames['divPrincipal'].location.reload()
-                window.frames['myDIV2'].location.reload()
-                window.frames['myDIV3'].location.reload()
              
+            }
+            else{
+                window.frames['myDIV2'].location.reload();
+                window.frames['myDIV2'].location.reload();
             }
             if (x.style.display === "none") {
                 
-                window.frames['divPrincipal'].location.reload()
+               
                 window.frames['myDIV2'].location.reload()
+                
+            }
+            else{
+                window.frames['divPrincipal'].location.reload()
                 window.frames['myDIV3'].location.reload()
             }
             if (g.style.display === "none") {
+               
+                window.frames['myDIV3'].location.reload()
+            }
+            else{
                 window.frames['divPrincipal'].location.reload()
                 window.frames['myDIV2'].location.reload()
-                window.frames['myDIV3'].location.reload()
             }
              
            }
