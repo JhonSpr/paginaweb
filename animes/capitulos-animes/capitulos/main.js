dab71f13304e4a3a4eda27d7ae26b4f92794c170
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-23 19:42:14 -0500

    a

--- animes/capitulos-animes/capitulos/main.js
+++ animes/capitulos-animes/capitulos/main.js
@@ -20,6 +20,9 @@ addEventListener('DOMContentLoaded', () => {
                var x = document.getElementById("myDIV2");
                var g = document.getElementById('myDIV3');
                var i = document.getElementById('divPrincipal');
+               var i = document.getElementById('divPrincipal').contentWindow.location.reload();
+               var g = document.getElementById('myDIV3').contentWindow.location.reload();
+               var x = document.getElementById('myDIV2').contentWindow.location.reload();
 
                if (i.style.display === "none") {
                    i.style.display = "block";
@@ -43,7 +46,9 @@ addEventListener('DOMContentLoaded', () => {
                var x = document.getElementById("myDIV2");
                var g = document.getElementById('myDIV3');
                var i = document.getElementById('divPrincipal');
-               var i = document.getElementById('myDIV3').contentWindow.location.reload();
+               var i = document.getElementById('divPrincipal').contentWindow.location.reload();
+               var g = document.getElementById('myDIV3').contentWindow.location.reload();
+               var x = document.getElementById('myDIV2').contentWindow.location.reload();
 
                if (g.style.display === "none") {
                    x.style.display = "none";
@@ -66,7 +71,10 @@ addEventListener('DOMContentLoaded', () => {
                var x = document.getElementById("myDIV2");
                var g = document.getElementById('myDIV3');
                var i = document.getElementById('divPrincipal');
-               var i = document.getElementById('myDIV2').contentWindow.location.reload();
+               var i = document.getElementById('divPrincipal').contentWindow.location.reload();
+               var g = document.getElementById('myDIV3').contentWindow.location.reload();
+               var x = document.getElementById('myDIV2').contentWindow.location.reload();
+
                if (x.style.display === "none") {
                    x.style.display = "block";
                    g.style.display="none";
