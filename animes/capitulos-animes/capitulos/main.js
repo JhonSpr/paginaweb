904164e1f39dcca08ee7e7f9530b2cbf1dfaffc8
Jhon <107087139+JhonSpr@users.noreply.github.com>
2022-07-25 21:51:35 -0500

    a

--- animes/capitulos-animes/capitulos/main.js
+++ animes/capitulos-animes/capitulos/main.js
@@ -134,14 +134,14 @@ addEventListener('DOMContentLoaded', () => {
             like = document.querySelector('.fa-thumbs-up');
              if (btnLike) {
                  btnLike.addEventListener('click', () =>{
-                    like.classList.add('likeado')
-                    btnLike.classList.add('likeadoBtn')
+                     like.style.color="#000";
+                     btnLike.style.background="#000";
                  });
  
              }
              else{
-                like.classList.remove('likeado')
-                btnLike.classList.remove('likeadoBtn')
+                btnLike.style.color="#000";
+                 like.style.color="#FF3636";
              }
  
              
